cdb05fdd8a4baa9b86bb2290d553243d6ebbcfb7
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2022-05-24 19:27:44 -0400

    Week 3 Initial Code Setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use('/', function main(req, res) {
+    res.send('Hello World');
+});
+
+app.listen(3000);
+
+console.log('Server running at http://localhost:3000/');
+
+module.exports = app;
+
